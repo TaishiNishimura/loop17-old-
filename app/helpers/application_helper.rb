9f9d1bc16ffde83c9f5b46420a6a055fcb52d77c
TaishiNishimura <farmers_trust_jp@yahoo.co.jp>
2021-09-06 07:28:21 +0000

    [feature]top page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def resource_name
+     :home
+  end
+
+  def resource
+     @resource ||= Home.new
+  end
+
+  def devise_mapping
+     @devise_mapping ||= Devise.mappings[:home]
+  end
 end
